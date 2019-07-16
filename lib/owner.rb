8b1c29f779e906ee6e00382818fc6bb8c281a3f7
Morgan Stanley <morganpstanley@gmail.com>
2019-07-16 08:09:48 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,74 @@
+require 'pry'
+
 class Owner
-  # code goes here
+  attr_reader :name, :species, :cats, :dogs
+
+  @@all = []
+  @@count = 0
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @cats = []
+    @dogs = []
+    @@all << self
+    @@count += 1
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def add_cat(cat)
+    @cats << cat
+  end
+
+  def add_dog(dog)
+    @dogs << dog
+  end
+
+  def buy_cat(name)
+    cat = Cat.new(name, self)
+    @cats << cat
+  end
+
+  def buy_dog(name)
+    dog = Dog.new(name, self)
+    @dogs << dog
+  end
+
+  def feed_cats
+    @cats.each { |cat| cat.mood = "happy"}
+  end
+
+  def walk_dogs
+    @dogs.each { |dog| dog.mood = "happy"}
+  end
+
+  def sell_pets
+    @dogs.each { |dog| dog.mood = "nervous"}
+    @cats.each { |cat| cat.mood = "nervous"}
+    @dogs.each { |dog| dog.owner = nil}
+    @cats.each { |cat| cat.owner = nil}
+    @cats.clear
+    @dogs.clear
+  end
+
+  def list_pets
+    "I have #{@dogs.length} dog(s), and #{@cats.length} cat(s)."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.reset_all
+    self.all.clear
+    @@count = 0
+  end
+
 end
